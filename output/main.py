bdc5b747da20e4860865db4b791778f2a00432dd
Peter Huynh <peterhyh@yahoo.com>
2023-12-08 11:34:26 -0600

    directory and file locations updated

--- output/main.py
+++ output/main.py
@@ -1,15 +1,15 @@
 # Run this file and the generated messages will be located in the ReadyToSend folder.
 
 
-with open("/Users/PeterHuynh/Desktop/Coding/Python/email_automation/input/template.txt") as data:
+with open("../input/template.txt") as data:
     template = data.read()
 
 string_to_replace = "[name]"
 
-with open("/Users/PeterHuynh/Desktop/Coding/Python/email_automation/input/names/names.txt") as data:
+with open("../input/names/names.txt") as data:
     name_list = data.read().split()
 
 for name in name_list:
     output = template.replace(string_to_replace, name)
-    with open(f"/Users/PeterHuynh/Desktop/Coding/Python/email_automation/output/ReadyToSend/{name}.txt", mode="w") as data:
+    with open(f"./ReadyToSend/{name}.txt", mode="w") as data:
         data.write(output)
